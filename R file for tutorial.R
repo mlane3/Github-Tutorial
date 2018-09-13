fec365247b40bed9cf0673f15c0f13952a2e9b38
mlane3 <mlane3@gatech.edu>
2018-09-13 19:07:25 -0400

    update
    update

---
+++ R file for tutorial.R
@@ -0,0 +1,15 @@
+## Insert comments here ----
+
+setwd("~/Homework/hw1") #comment out or remove this line.
+#One issue with setting a working directory that people don't have your computer.
+#We can use projects instead to manage github repositories. 
+
+# Calculate 3 + 4
+
+# Calculate 6 + 12
+
+# Print Hello World
+
+# Read credit card data.
+library(readr)
+read_table2(file = "")
